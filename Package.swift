d56fb7314eb10f29f8d54b1ce4712c3ec1d4b932
Ruturaj Choudhari <ruturaj.choudhari@front.com>
2025-03-25 16:53:11 -0700

    Release version 0.0.0-beta.1

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "FrontSdk",
-            url: "https://github.com/frontapp/front-ios-sdk-swift-package-manager/releases/download/0.0.0-alpha.6/FrontSdk-0.0.0-alpha.6.zip",
-            checksum: "006666ec9b8f28e217ab908dad22a45426375315b7e9d1440af7714c30ea5d4d"
+            url: "https://github.com/frontapp/front-ios-sdk-swift-package-manager/releases/download/0.0.0-beta.1/FrontSdk-0.0.0-beta.1.zip",
+            checksum: "35570306d606477c935887627504c0b8c2cdaf69cbeb22b3fd25ee4832202fa4"
         ),
         .testTarget(
             name: "FrontSdkTests",
